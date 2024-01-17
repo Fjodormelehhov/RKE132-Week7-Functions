dd1677512343b9f52a9406fbcb31ea4cbd264615
Fjodormelehhov <fjodor.melehhov@tktk.ee>
2024-01-17 15:09:59 +0200

    add Functions code

---
+++ ConverterTemp/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("What you want to convert: Celcius or Fahrenheit - F/C :");
+char userChoice = Char.Parse(Console.ReadLine());
+
+
+int userInput = Int32.Parse(Console.ReadLine());
+
+switch (userChoice)
+{
+    case 'F':
+        ConvertToCelcius(userInput);
+        break;
+    case 'C':
+        ConvertToFahrenheit(userInput);
+        break;
+}
+
+
+static void ConvertToCelcius(int celsius)
+{
+    Console.WriteLine($"{(celsius * 9) / 5 + 32}");
+}
+
+static void ConvertToFahrenheit(int fahrenheit)
+{
+    Console.WriteLine($"{((fahrenheit - 32) * 5 / 9)}");
+}
